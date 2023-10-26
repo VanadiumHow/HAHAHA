eda317dca84b060a02d4a5b26d5d02888387b71c
XuHF <XuHF@DESKTOP-EHGPD9O>
2023-10-26 16:35:09 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        //readonly int bb;
+        //Program()
+        //{
+        //    bb = 1;
+        //}
+        static void Main(string[] args)
+        {
+            #region 条件运算符
+            //const int aa = 1;
+            //Program p = new Program();
+            //string asd = $"this is a : {aa}";
+            //int x = 5, y = 6, z;
+            //z = x < y ? 3 : 4;
+            //int j = Convert.ToInt32(x);
+            //string bb = "asdasd";
+            //j = int.Parse(bb);
+            #endregion
+
+
+            #region 根据金钱算出公鸡母鸡雏鸡各有多少只
+            //Console.WriteLine("一只公鸡5文钱，一只母鸡3文钱，俩只雏鸡1文钱，请输入当前拥有钱币的数量，即可为你算出能够购买公鸡、母鸡、雏鸡的所有搭配");
+            //string InputCoinString = Console.ReadLine();
+            //int InputCoin = Convert.ToInt32(InputCoinString);
+            //InputCoin = int.Parse(InputCoinString);
+            //int Cock, Hen, Chick;
+            //Console.WriteLine("可能的情况为：");
+            //for (int i = 0; i <= InputCoin / 5; i++)
+            //{
+            //    Cock = InputCoin / 5 - i;
+            //    for (int j = 0; j <= (InputCoin - Cock * 5) / 3; j++)
+            //    {
+            //        Hen = (InputCoin - Cock * 5) / 3 - j;
+            //        Chick = (InputCoin - Cock * 5 - Hen * 3) * 2;
+            //        Console.WriteLine("公鸡：{0}只，母鸡：{1}只，雏鸡：{2}只。", Cock, Hen, Chick);
+            //    }
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region 数组
+            //int[] ArrA = new int[3];
+            //int[] ArrB = new int[] { 1, 2, 3 };
+            //int[] ArrC = { 1, 2, 3, };
+            //int[,] ArrD = new int[2, 3];
+            //int[][] ArrE = new int[2][];
+            //ArrE[0] = new int[2];
+            //ArrE[1] = new int[3];
+            //string s = string.Empty;
+            //s = Console.ReadLine();
+            //String[] ss = s.Split(',');
+            //Array.Sort(ss);   //将数组进行从小到大排序
+            //Array.Reverse(ss);    //反转数组元素
+            #endregion
+
+            #region 字符串
+            //char[] charArr = { '时', '间', '就', '是', '金', '钱' };
+            //string Str = new string(charArr, 4, 2);
+            //string str1 = "努力工作";
+            //string str2 = "Keep up";
+            //char ch = str1[2];
+            //bool result1 = str1.StartsWith("努力");
+            //bool result2 = str1.EndsWith("作");
+            //str2.StartsWith("keep", true, null);
+            //str1.Equals(str2);
+            //str2.Replace('K', 'k');
+
+            #endregion
+            
+            #region 
+
+            #endregion
+        }
+    } 
+}
